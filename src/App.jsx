2a72631735fde5f9ecbf6a786ccee7ffedec6f17
Galihnantion <galihsaputra3139@gmail.com>
2026-05-05 06:23:19 +0700

    fix: import useRef yang terlupakan untuk music player

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { 
